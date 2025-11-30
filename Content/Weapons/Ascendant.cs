191289fa40465387a62efab9d8bf6bc878963a0e
ldysena <ldysena@proton.me>
2025-11-30 00:08:54 -0600

    Initial commit

---
+++ Content/Weapons/Ascendant.cs
@@ -0,0 +1,8 @@
+namespace LaSangreMod.Content
+{
+	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
+	public class Ascendant : ModItem
+	{
+		
+	}
+}
