0df9a35dc07384845917e61cea31028ccef09a18
ldysena <ldysena@proton.me>
2026-02-04 20:04:53 -0600

    Setup template jousting lance
    Using examplemod's examplejoustinglance as a template, setup a jousting lance that builds and loads correctly

--- Content/Weapons/Ascendant.cs
+++
@@ -1,8 +0,0 @@
-namespace LaSangreMod.Content
-{
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class Ascendant : ModItem
-	{
-		
-	}
-}
