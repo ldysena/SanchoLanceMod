bf2a6b751a6b4e401b999618a5d2d217f48b72e5
ldysena <ldysena@proton.me>
2026-05-13 15:30:23 -0600

    Implement temporary Hardblood bar
    Bar position moved below player and given temp art

--- Common/SanchoHardbloodBar.cs
+++ Common/SanchoHardbloodBar.cs
@@ -18,7 +18,7 @@ namespace LaSangreMod.Common
 		// For this bar we'll be using a frame texture and then a gradient inside bar, as it's one of the more simpler approaches while still looking decent.
 		// Once this is all set up make sure to go and do the required stuff for most UI's in the ModSystem class.
 		private UIText text;
-		private UIElement area;
+		public UIElement area; // Temporarily make public for the sake of fixing this
 		private UIImage barFrame;
 		private Color gradientA;
 		private Color gradientB;
@@ -27,36 +27,36 @@ namespace LaSangreMod.Common
 			// Create a UIElement for all the elements to sit on top of, this simplifies the numbers as nested elements can be positioned relative to the top left corner of this element. 
 			// UIElement is invisible and has no padding.
 			area = new UIElement();
-			area.Left.Set(-area.Width.Pixels - 600, 1f); // Place the resource bar to the left of the hearts.
-			area.Top.Set(30, 0f); // Placing it just a bit below the top of the screen.
-			area.Width.Set(182, 0f); // We will be placing the following 2 UIElements within this 182x60 area.
-			area.Height.Set(60, 0f);
+			area.Left.Set(Main.screenWidth / 2 - 40, 0f); // Align the bar with the player
+			area.Top.Set(Main.screenHeight / 2 + 30, 0f); // Placing it just a bit below the player
+			area.Width.Set(40, 0f); 
+			area.Height.Set(20, 0f);
 
-			barFrame = new UIImage(ModContent.Request<Texture2D>("LaSangreMod/Common/ExampleResourceFrame")); // Frame of our resource bar
+			barFrame = new UIImage(ModContent.Request<Texture2D>("LaSangreMod/Common/temphardbloodbar")); // Frame of our resource bar
 			barFrame.Left.Set(22, 0f);
 			barFrame.Top.Set(0, 0f);
-			barFrame.Width.Set(138, 0f);
-			barFrame.Height.Set(34, 0f);
+			barFrame.Width.Set(36, 0f);
+			barFrame.Height.Set(16, 0f);
 
-			text = new UIText("0/0", 0.8f); // text to show stat
+			/*text = new UIText("0/0", 0.8f); // text to show stat
 			text.Width.Set(138, 0f);
 			text.Height.Set(34, 0f);
 			text.Top.Set(40, 0f);
-			text.Left.Set(0, 0f);
+			text.Left.Set(0, 0f);*/
 
-			gradientA = new Color(123, 25, 138); // A dark purple
-			gradientB = new Color(187, 91, 201); // A light purple
+			gradientA = new Color(131, 0, 70); // A dark purple
+			gradientB = new Color(255, 73, 73); // Bright red
 
-			area.Append(text);
+			//area.Append(text);
 			area.Append(barFrame);
 			Append(area);
 		}
 
-		public override void Draw(SpriteBatch spriteBatch) {
-			// This prevents drawing unless we are using an ExampleCustomResourceWeapon
-			if (Main.LocalPlayer.HeldItem.ModItem is not LaSangreWeapon &&
-			    Main.LocalPlayer.HeldItem.ModItem is not AscendantWeapon)
-				return;
+		public override void Draw(SpriteBatch spriteBatch) 
+		{
+			if (!Main.LocalPlayer.HasItem(ModContent.ItemType<LaSangreWeapon>()) &&
+			    !Main.LocalPlayer.HasItem(ModContent.ItemType<AscendantWeapon>()))
+				{ return; }
 
 			base.Draw(spriteBatch);
 		}
@@ -72,10 +72,10 @@ namespace LaSangreMod.Common
 
 			// Here we get the screen dimensions of the barFrame element, then tweak the resulting rectangle to arrive at a rectangle within the barFrame texture that we will draw the gradient. These values were measured in a drawing program.
 			Rectangle hitbox = barFrame.GetInnerDimensions().ToRectangle();
-			hitbox.X += 12;
-			hitbox.Width -= 24;
-			hitbox.Y += 8;
-			hitbox.Height -= 16;
+			hitbox.X += 6;
+			hitbox.Width -= 12;
+			hitbox.Y += 6;
+			hitbox.Height -= 12;
 
 			// Now, using this hitbox, we draw a gradient by drawing vertical lines while slowly interpolating between the 2 colors.
 			int left = hitbox.Left;
@@ -88,22 +88,23 @@ namespace LaSangreMod.Common
 			}
 		}
 
-		public override void Update(GameTime gameTime) {
+		/*public override void Update(GameTime gameTime) {
 			if (Main.LocalPlayer.HeldItem.ModItem is not LaSangreWeapon &&
 			    Main.LocalPlayer.HeldItem.ModItem is not AscendantWeapon)
 				return;
 
 			var modPlayer = Main.LocalPlayer.GetModPlayer<SanchoModPlayer>();
 			// Setting the text per tick to update and show our resource values.
-			text.SetText(ExampleResourceUISystem.ExampleResourceText.Format(modPlayer.hardblood, SanchoModPlayer.HARDBLOOD_MAX));
+			//text.SetText(ExampleResourceUISystem.ExampleResourceText.Format(modPlayer.hardblood, SanchoModPlayer.HARDBLOOD_MAX));
 			base.Update(gameTime);
-		}
+		}*/
 	}
 
 	// This class will only be autoloaded/registered if we're not loading on a server
 	[Autoload(Side = ModSide.Client)]
 	internal class ExampleResourceUISystem : ModSystem
 	{
+		
 		private UserInterface ExampleResourceBarUserInterface;
 
 		internal SanchoHardbloodBar ExampleResourceBar;
@@ -119,6 +120,26 @@ namespace LaSangreMod.Common
 			ExampleResourceText ??= Mod.GetLocalization($"{category}.ExampleResource");
 		}
 
+		// Event handling to keep the bar under the player when changing the screen size
+		// TODO: Make helper method instead of public vars?
+		public void ResetHardbloodBar(Vector2 v)
+		{
+			ExampleResourceBar.area.Left.Set(Main.screenWidth / 2 - 40, 0f); // Align the bar with the player
+			ExampleResourceBar.area.Top.Set(Main.screenHeight / 2 + 30, 0f); // Placing it just a bit below the player
+		}
+
+        public override void OnModLoad()
+        {
+			Main.OnResolutionChanged += ResetHardbloodBar;
+            base.OnModLoad();
+        }
+
+		public override void OnModUnload()
+         {
+			Main.OnResolutionChanged -= ResetHardbloodBar;
+            base.OnModUnload();
+        }
+ 
 		public override void UpdateUI(GameTime gameTime) {
 			ExampleResourceBarUserInterface?.Update(gameTime);
 		}
